4d6bcc6d110e036eef235c18cfc82f9b5269cb49
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-16 13:13:51 -0300

    Method to update category

--- src/main/java/com/fernandoalencar/algamoneyapi/controller/CategoriaController.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/controller/CategoriaController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,19 @@ public class CategoriaController {
 		
 		return categoria.isPresent() ?  ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
 
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Categoria> atualizar(@PathVariable Long id, @RequestBody Categoria categoria){
+		
+		if (!categoriaRepository.existsById(id)) {
+			return ResponseEntity.notFound().build(); 
+		}
+		
+		categoria.setId(id);
+		categoriaRepository.save(categoria);
+		
+		return ResponseEntity.ok(categoria);
 	}
 	
 	@DeleteMapping("/{id}")
