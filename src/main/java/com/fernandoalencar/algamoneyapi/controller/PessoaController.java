6c3775f56de3a0f96ca5b06ee0a9511044e2cfb5
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-16 16:12:34 -0300

    Method to list people by id

--- src/main/java/com/fernandoalencar/algamoneyapi/controller/PessoaController.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/controller/PessoaController.java
@@ -2,13 +2,17 @@ package com.fernandoalencar.algamoneyapi.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,7 +29,7 @@ public class PessoaController {
 	private PessoaRepository pessoaRepository;
 	
 	@PostMapping
-	public ResponseEntity<Pessoa> cadastrar(@RequestBody Pessoa pessoa, HttpServletResponse response){
+	public ResponseEntity<Pessoa> cadastrar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response){
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(pessoaSalva.getId()).toUri();
@@ -37,5 +41,14 @@ public class PessoaController {
 	@GetMapping
 	public List<Pessoa> listar(){
 		return pessoaRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Pessoa> buscar(@PathVariable Long id){
+		
+		Optional<Pessoa> pessoa = pessoaRepository.findById(id);
+		
+		return pessoa.isPresent() ? ResponseEntity.ok(pessoa.get()) : ResponseEntity.notFound().build();
+		
 	}
 }
