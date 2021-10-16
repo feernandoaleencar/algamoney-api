9e86ba62aa019af243792d55ff16b45b9a00ac89
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-16 01:29:18 -0300

    Method to list category by id

--- src/main/java/com/fernandoalencar/algamoneyapi/controller/categoriaController.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/controller/categoriaController.java
@@ -2,12 +2,14 @@ package com.fernandoalencar.algamoneyapi.controller;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +39,14 @@ public class categoriaController {
 	@GetMapping
 	public List<Categoria> listar(){
 		return categoriaRepository.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Categoria> buscar(@PathVariable Long id) {
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		
+		return categoria.isPresent() ?  ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
+
 	}
 	
 }
