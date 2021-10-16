10c9a77f5e9ab14f2a47dbd6fd172f2545c76f98
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-16 15:37:39 -0300

    creted controller people

---
+++ src/main/java/com/fernandoalencar/algamoneyapi/controller/PessoaController.java
@@ -0,0 +1,19 @@
+package com.fernandoalencar.algamoneyapi.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fernandoalencar.algamoneyapi.model.Pessoa;
+import com.fernandoalencar.algamoneyapi.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+	
+
+	
+}
