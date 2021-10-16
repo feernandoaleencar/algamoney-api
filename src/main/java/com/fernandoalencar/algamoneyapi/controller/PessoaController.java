06b34c72167171860dc44ba59e7a7acc9eb4c2a1
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-16 15:49:18 -0300

    Method to save people

--- src/main/java/com/fernandoalencar/algamoneyapi/controller/PessoaController.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/controller/PessoaController.java
@@ -1,11 +1,18 @@
 package com.fernandoalencar.algamoneyapi.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.fernandoalencar.algamoneyapi.model.Pessoa;
 import com.fernandoalencar.algamoneyapi.repository.PessoaRepository;
@@ -14,6 +21,17 @@ import com.fernandoalencar.algamoneyapi.repository.PessoaRepository;
 @RequestMapping("/pessoas")
 public class PessoaController {
 	
-
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+	@PostMapping
+	public ResponseEntity<Pessoa> cadastrar(@RequestBody Pessoa pessoa, HttpServletResponse response){
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(pessoaSalva.getId()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(pessoaSalva);
+	}
 	
 }
