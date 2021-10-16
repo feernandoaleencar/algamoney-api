5cf55d10b805d4330b37e31e4b73e146de1f6db0
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-16 02:09:09 -0300

    Method to delete category

--- src/main/java/com/fernandoalencar/algamoneyapi/controller/CategoriaController.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/controller/CategoriaController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +22,7 @@ import com.fernandoalencar.algamoneyapi.repository.CategoriaRepository;
 
 @RestController
 @RequestMapping("/categorias")
-public class categoriaController {
+public class CategoriaController {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
@@ -47,6 +48,18 @@ public class categoriaController {
 		
 		return categoria.isPresent() ?  ResponseEntity.ok(categoria.get()) : ResponseEntity.notFound().build();
 
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id){
+		
+		 if (!categoriaRepository.existsById(id)) {
+			return ResponseEntity.notFound().build();		
+		 }
+		 
+		 categoriaRepository.deleteById(id);
+		 
+		 return ResponseEntity.noContent().build();
 	}
 	
 }
