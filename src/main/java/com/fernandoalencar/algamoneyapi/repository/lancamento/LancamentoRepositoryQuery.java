95a130f2a9b5d81182ba237e8c9afecc49405094
Fernando Alencar <feernando_aleencar@hotmail.com>
2021-10-26 22:31:53 -0300

    adicionando spring security ao projeto

--- src/main/java/com/fernandoalencar/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,11 +1,12 @@
 package com.fernandoalencar.algamoneyapi.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.fernandoalencar.algamoneyapi.model.Lancamento;
 import com.fernandoalencar.algamoneyapi.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 	
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
