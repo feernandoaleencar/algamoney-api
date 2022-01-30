b76436ebacefd221e12ea2808f2f5c5c4933296d
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-29 23:40:45 -0300

    Criando usuarioRepository

---
+++ src/main/java/com/fernandoalencar/algamoneyapi/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.fernandoalencar.algamoneyapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.fernandoalencar.algamoneyapi.model.Usuario;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    public Optional<Usuario> findByEmail(String email);
+}
