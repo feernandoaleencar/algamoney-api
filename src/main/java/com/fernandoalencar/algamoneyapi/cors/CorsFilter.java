042a0dc01021783698e88e2031194b1c9cc09ed9
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-29 23:19:30 -0300

    Criando filtro para CORS

---
+++ src/main/java/com/fernandoalencar/algamoneyapi/cors/CorsFilter.java
@@ -0,0 +1,29 @@
+package com.fernandoalencar.algamoneyapi.cors;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
