5ff1231b0fd785f09e5cb0f6bd1285f52bdacf4c
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-29 23:26:32 -0300

    Criando filtro para CORS

--- src/main/java/com/fernandoalencar/algamoneyapi/cors/CorsFilter.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/cors/CorsFilter.java
@@ -12,6 +12,10 @@ import java.io.IOException;
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
+
+    private String originPermitida = "http://localhost:8000"; // TODO: Configurar para diferentes ambientes
+
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
         Filter.super.init(filterConfig);
@@ -19,11 +23,26 @@ public class CorsFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) resp;
 
+        response.setHeader("Access-Control-Allow-Origin", originPermitida);
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+
+        if ("OPTIONS".equals(request.getMethod()) && originPermitida.equals(request.getHeader("Origin"))) {
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+            response.setHeader("Access-Control-Max-Age", "3600");
+
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
     }
 
     @Override
     public void destroy() {
         Filter.super.destroy();
     }
+
 }
