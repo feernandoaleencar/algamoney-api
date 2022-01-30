655b638d7a249212e852a57f46e0635cfbc98853
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-29 23:51:23 -0300

    Criando AppUserDetailsService

--- src/main/java/com/fernandoalencar/algamoneyapi/security/AppUserDetailsService.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/security/AppUserDetailsService.java
@@ -1,15 +1,38 @@
 package com.fernandoalencar.algamoneyapi.security;
 
+import com.fernandoalencar.algamoneyapi.model.Usuario;
+import com.fernandoalencar.algamoneyapi.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+
 @Service
 public class AppUserDetailsService implements UserDetailsService {
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<Usuario> usuarioOptional = usuarioRepository.findByEmail(email);
+        Usuario usuario = usuarioOptional.orElseThrow(() -> new UsernameNotFoundException("Usuário e/ou senha inválidos"));
+
+        return new User(email, usuario.getSenha(), getPermissao(usuario));
+    }
+
+    private Collection<? extends GrantedAuthority> getPermissao(Usuario usuario) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        usuario.getPermissoes().forEach(p -> authorities.add(new SimpleGrantedAuthority(p.getDescricao().toUpperCase())));
+        return authorities;
     }
 }
