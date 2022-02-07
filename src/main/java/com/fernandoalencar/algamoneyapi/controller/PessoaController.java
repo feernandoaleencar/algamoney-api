ed6f74731322d76f05239cb76f590b5a81ac9d32
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-06 23:08:29 -0300

    Alterando endpoint de listar pessoas com filtro por nome

--- src/main/java/com/fernandoalencar/algamoneyapi/controller/PessoaController.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/controller/PessoaController.java
@@ -8,18 +8,12 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.fernandoalencar.algamoneyapi.event.RecursoCriadoEvent;
 import com.fernandoalencar.algamoneyapi.model.Pessoa;
@@ -51,8 +45,8 @@ public class PessoaController {
 	
 	@GetMapping
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA') and hasAuthority('SCOPE_read')")
-	public List<Pessoa> listar(){
-		return pessoaRepository.findAll();
+	public Page<Pessoa> listar(@RequestParam(required = false, defaultValue = "") String nome, Pageable pageable) {
+		return pessoaRepository.findByNomeContaining(nome, pageable);
 	}
 	
 	@GetMapping("/{id}")
--- src/main/java/com/fernandoalencar/algamoneyapi/repository/PessoaRepository.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/repository/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.fernandoalencar.algamoneyapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fernandoalencar.algamoneyapi.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+    public Page<Pessoa> findByNomeContaining(String nome, Pageable pageable);
 
 }
