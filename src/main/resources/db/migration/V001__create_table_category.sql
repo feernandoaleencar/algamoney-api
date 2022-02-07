348881ab22cf86596a57757b2d0c1b2d17567566
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-06 23:09:06 -0300

    Ajustando scripts banco de dados

--- src/main/resources/db/migration/V001__create_table_category.sql
+++ src/main/resources/db/migration/V001__create_table_category.sql
@@ -1,4 +1,16 @@
-CREATE TABLE categoria (
-	id bigint(20) primary key auto_increment,
+CREATE TABLE categoria
+(
+    id   bigint(20) primary key auto_increment,
     nome varchar(60) not null
-) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO categoria (nome)
+values ('Lazer');
+INSERT INTO categoria (nome)
+values ('Alimentação');
+INSERT INTO categoria (nome)
+values ('Supermercado');
+INSERT INTO categoria (nome)
+values ('Farmácia');
+INSERT INTO categoria (nome)
+values ('Outros');
