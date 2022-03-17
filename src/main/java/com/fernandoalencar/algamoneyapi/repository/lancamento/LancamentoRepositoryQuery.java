28e4321ebb05474de9d7fcc234633136b6bd5d36
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-16 23:32:14 -0300

    Criando metodo de consulta para dados por categoria no repository

--- src/main/java/com/fernandoalencar/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,5 +1,6 @@
 package com.fernandoalencar.algamoneyapi.repository.lancamento;
 
+import com.fernandoalencar.algamoneyapi.dto.LancamentoEstatisticaCategoria;
 import com.fernandoalencar.algamoneyapi.repository.projection.ResumoLancamento;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,8 +8,13 @@ import org.springframework.data.domain.Pageable;
 import com.fernandoalencar.algamoneyapi.model.Lancamento;
 import com.fernandoalencar.algamoneyapi.repository.filter.LancamentoFilter;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface LancamentoRepositoryQuery {
 	
 	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	public Page<ResumoLancamento> resumir(LancamentoFilter lancamentoFilter, Pageable pageable);
+
+	public List<LancamentoEstatisticaCategoria> porCategoria(LocalDate mesReferencia);
 }
