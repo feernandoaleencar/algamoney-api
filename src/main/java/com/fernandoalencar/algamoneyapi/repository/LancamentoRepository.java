21194179a936010f3999b3daca325451dc4654f3
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-22 23:52:27 -0300

    Buscando lançamentos vencidos com Spring Data JPA

--- src/main/java/com/fernandoalencar/algamoneyapi/repository/LancamentoRepository.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/repository/LancamentoRepository.java
@@ -5,6 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fernandoalencar.algamoneyapi.model.Lancamento;
 import com.fernandoalencar.algamoneyapi.repository.lancamento.LancamentoRepositoryQuery;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 
+    List<Lancamento> findbyDataVencimentoLessThanEqualsAndDataPagamentoIsNull(LocalDate data);
 }
--- src/main/java/com/fernandoalencar/algamoneyapi/repository/UsuarioRepository.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/repository/UsuarioRepository.java
@@ -3,9 +3,12 @@ package com.fernandoalencar.algamoneyapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.fernandoalencar.algamoneyapi.model.Usuario;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
     public Optional<Usuario> findByEmail(String email);
+
+    public List<Usuario> findByPermissoesDescricao(String permissoesDescricao);
 }
