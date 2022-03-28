e91199187723f7cfe5b8b8537a232ff53d4f2ea0
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-28 15:28:31 -0300

    Implementando o envio do arquivo para o S3

--- src/main/java/com/fernandoalencar/algamoneyapi/storage/S3.java
+++ src/main/java/com/fernandoalencar/algamoneyapi/storage/S3.java
@@ -46,7 +46,6 @@ public class S3 {
 
             putObjectRequest.setTagging(new ObjectTagging(
                     Arrays.asList(new Tag("expirar", "true"))));
-            ));
 
             amazonS3.putObject(putObjectRequest);
 
